ad35d502f01158ddb279ce0c84bada8053a8cc98
Nemesis100010 <168680358+Nemesis100010@users.noreply.github.com>
2024-08-22 17:28:06 +0000

    container config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,2 +1,13 @@
 const accountId = "133452"
+let accountEmail = "som2000@google.com"
+var accountPassword = "34412"
+accountCity = "Barasat"
+//try not to use var 
+let accountState;
+//accountId=2 // it is not allowed to change const variable as we are using nodejs not browser
+accountEmail="nomoney@gmail.com"
+accountPassword="12345"
+accountCity ="Midnpore"
 console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+//accountState shows undefined as its value isn't given
