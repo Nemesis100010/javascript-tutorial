5c8ea2f69ef28b766dd2b629a8067f66d49d2ec4
Nemesis100010 <168680358+Nemesis100010@users.noreply.github.com>
2024-08-25 10:13:06 +0000

    03_conversionOperation.js

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "33"
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+//"33"=>33
+//"33abc"=> NaN
+//true=>1; false=>0
+//null=>0 ; undefined=>NaN
+let isLoggedIn = 1
+let valueInBoolean = Boolean(isLoggedIn)
+console.log(typeof valueInBoolean)
+console.log(valueInBoolean)
+//1=>true 0=>false
+//""=>false 
+//"Som"=>true
+let someNumber = 0
+let numberInString = String(someNumber)
+console.log(typeof numberInString)
+console.log(numberInString)
+
