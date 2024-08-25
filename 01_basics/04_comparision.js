d206843ca4b508d9f43e1b04ad2434d7aee43ab3
Nemesis100010 <168680358+Nemesis100010@users.noreply.github.com>
2024-08-25 15:38:48 +0000

    container config done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,9 @@
+//console.log(2>1)
+//console.log(2>=1)
+//console.log(2<1)
+//console.log(2==1)
+//console.log(2!=1)
+console.log("2">1); //javascript is converting sring "2" to number 2 and comparing
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
