0cf033121aeed089390e546976cbe593eb366e67
Nemesis100010 <168680358+Nemesis100010@users.noreply.github.com>
2024-08-25 16:10:41 +0000

    container config done

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,11 @@
+//Primitive
+//7 types : String , Number , Boolean , null , undefined, Symbol, BigInt
+//Reference (non-primitive)
+//Array, objects, functions
+//Array
+const heros = ["Saktiman","naagraj","doga"];
+console.log(heros)
+let myObj = {
+    name : "Som"
+    age : 22
+}
