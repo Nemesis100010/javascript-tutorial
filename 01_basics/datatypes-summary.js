306c17e6eeb995fd7dc247566072784db9ec2ce7
Nemesis100010 <168680358+Nemesis100010@users.noreply.github.com>
2024-09-01 13:18:05 +0000

    datatype summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -2,10 +2,24 @@
 //7 types : String , Number , Boolean , null , undefined, Symbol, BigInt
 //Reference (non-primitive)
 //Array, objects, functions
-//Array
+//Array 
+const ousideTemp = null
+const bigNumber = 347658955456345n
 const heros = ["Saktiman","naagraj","doga"];
 console.log(heros)
 let myObj = {
-    name : "Som"
-    age : 22
+    name : "som",
+    age:22
 }
+const myFunction = function(){
+    console.log("Hello World");
+}
+console.log(typeof bigNumber);//bigint
+console.log(typeof myFunction);//function
+console.log(typeof myObj);//object
+console.log(typeof ousideTemp);//object 
+console.log(typeof heros);//object
+
+
+
+
