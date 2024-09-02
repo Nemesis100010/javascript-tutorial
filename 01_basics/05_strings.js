c8897281c591b7d2c3326ba85e9966aec2621bd9
Nemesis100010 <168680358+Nemesis100010@users.noreply.github.com>
2024-09-02 15:01:17 +0000

    container config done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,10 @@
+const name = "whitedevil"
+const num = 50
+//console.log(name + num + "value" );
+console.log(`Hello my name is ${name} and my number is ${num}`);
+const gameName = new String('koustuve')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
