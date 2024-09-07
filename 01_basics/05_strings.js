3aaf968062888a7784b15de058db420d0f361fe3
Nemesis100010 <168680358+Nemesis100010@users.noreply.github.com>
2024-09-07 16:13:47 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,10 +1,16 @@
-const name = "whitedevil"
-const num = 50
-//console.log(name + num + "value" );
-console.log(`Hello my name is ${name} and my number is ${num}`);
-const gameName = new String('koustuve')
-console.log(gameName[0]);
-console.log(gameName.__proto__);
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
+const name = "koustuve"
+const favnum = 50
+console.log(name+favnum+"value");
+console.log(`Hello my name is ${name} and my favourite number is ${favnum}`)
+const gamename = new String ('Fortnight')
+console.log(gamename)
+console.log(gamename[2]);
+console.log(gamename.__proto__);
+console.log(gamename.length);
+console.log(gamename.toUpperCase());
+console.log(gamename.charAt(4));
+console.log(gamename.indexOf("g"));
+const newString = gamename.substring(0,4)
+console.log(newString);
+
 
