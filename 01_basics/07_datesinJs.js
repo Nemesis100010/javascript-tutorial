fa1ecf0da04365c00b2f4c231e4c4b2130da5ca5
Nemesis100010 <168680358+Nemesis100010@users.noreply.github.com>
2024-09-17 15:58:42 +0000

    datesinjs

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,22 @@
+//Dates
+let myDate = new Date()
+//console.log(myDate.toString());
+//console.log(myDate.toDateString());
+//console.log(myDate.toISOString());
+//console.log(typeof myDate);
+//let myCreatedDate = new Date(2002,8,30)
+//console.log(newDate.toLocaleDateString());
+//let newDate = new Date("2024-02-03")
+//console.log(newDate.toLocaleDateString());
+let newDate = new Date("02-03-2024");
+console.log(newDate.toDateString());
+//let myTimeStamp = Date.now()
+//console.log(myTimeStamp);
+//console.log(newDate.getTime());
+console.log(Math.round(Date.now()/1000));
+console.log(newDate.getFullYear());
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDate());
+
+
+
